f36730f5abc4b5a249bc3497b6239697c70a0b0b
Swarm Agent 60 <ceo@walker.com>
2026-09-22 14:37:25 -0400

    Initial commit for Walker General Contractors

---
+++ src/components/GoogleMapsSEO.jsx
@@ -0,0 +1,33 @@
+﻿import React from 'react';
+
+export default function GoogleMapsSEO({ lang = 'en' }) {
+  // Uses dynamic window.location.origin if in browser, otherwise uses a placeholder
+  const getBaseUrl = () => typeof window !== 'undefined' ? window.location.origin : 'https://YOUR_FUTURE_DOMAIN.com';
+  
+  const getLocalizedSchema = () => {
+    let mapUrl = "https://maps.google.com/?cid=YOUR_CID_HERE&hl=en";
+    
+    switch(lang) {
+      case 'zh': mapUrl = "https://maps.google.com/?cid=YOUR_CID_HERE&hl=zh-CN"; break;
+      case 'fr': mapUrl = "https://maps.google.com/?cid=YOUR_CID_HERE&hl=fr"; break;
+      case 'hi': mapUrl = "https://maps.google.com/?cid=YOUR_CID_HERE&hl=hi"; break;
+      case 'ru': mapUrl = "https://maps.google.com/?cid=YOUR_CID_HERE&hl=ru"; break;
+      case 'fa': mapUrl = "https://maps.google.com/?cid=YOUR_CID_HERE&hl=fa"; break; // Iranian/Persian
+    }
+
+    return JSON.stringify({
+      "@context": "https://schema.org",
+      "@type": "Organization",
+      "name": "Global Digital Services",
+      "url": getBaseUrl(),
+      "hasMap": mapUrl
+    });
+  };
+
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: getLocalizedSchema() }}
+    />
+  );
+}
---
+++ src/i18n.js
@@ -0,0 +1,23 @@
+﻿import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+const resources = {
+  en: { translation: { "welcome": "Welcome" } },
+  zh: { translation: { "welcome": "欢迎" } },
+  fr: { translation: { "welcome": "Bienvenue" } },
+  hi: { translation: { "welcome": "स्वागत" } },
+  ru: { translation: { "welcome": "Добро пожаловать" } },
+  fa: { translation: { "welcome": "خوش آمدید" } } // Iranian / Persian
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    interpolation: { escapeValue: false }
+  });
+
+export default i18n;
